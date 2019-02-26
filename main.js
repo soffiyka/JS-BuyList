c9b5e17365e5a11c5bfd4c89fbb9b4a56edbdc86
Sofiya Gak <sofiya.gak@gmail.com>
2019-02-26 19:29:44 +0200

    [practice-3] 6. organized the editing of the products' amount; hung events on plus and minus button; 7. added statistics to the right column

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
 $(function () {
     function addProd(name) {
         if (name) {
+            var count = 1;
+
             //product in the left column
             var $newProd = $("#newProd").clone();
             $newProd.removeAttr('id');
             $newProd.find(".for-add").text(name);
             $('.left').append($newProd);
 
-
             //clean the input
             $('.nameinput').val('');
+
+            //statistics on the right
+            var $prodLeft = $('#prodLeft').clone();
+            $prodLeft.removeAttr('id');
+            $prodLeft.find('.right-prodName').text(name);
+            $('#leftover').append($prodLeft);
+
+
+            //event on minus button
+            var $minus = $newProd.find('.minBut');
+            $minus.click(function () {
+                count--;
+                if(count==1){
+                    $minus.css('background-color', 'rgba(226, 61, 61, 0.6)');
+                    $minus.css('cursor', 'auto');
+                }
+                else if(count<1)
+                    count++;
+                $newProd.find('.leftamount').text(count);
+                $prodLeft.find('.rightamount').text(count);
+
+            });
+            //event on plus button
+            var $plus = $newProd.find('.plusbut');
+            $plus.click(function(){
+               count++;
+               $newProd.find('.leftamount').text(count);
+               $prodLeft.find('.rightamount').text(count);
+               if(count>1){
+                   $minus.css('background-color', 'rgb(226, 61, 61)');
+                   $minus.css('cursor', 'pointer');
+               }
+            });
         }
     }
 
@@ -27,8 +61,9 @@ $(function () {
         }
     });
 
-    //3 items have already been added
+    //3 items have been already added
     addProd('Помідори');
     addProd('Печиво');
     addProd('Сир');
+
 });
