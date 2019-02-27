02d1559ec9cbdfec61ed1783ec159056196b3cc5
Sofiya Gak <sofiya.gak@gmail.com>
2019-02-27 20:14:26 +0200

    [practice-3] 4. enabled editing of the product name; disabled 'space' names for products

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 $(function () {
     function addProd(name) {
         if (name) {
+
             var count = 1;
 
             //product in the left column
             var $newProd = $("#newProd").clone();
             $newProd.removeAttr('id');
-            $newProd.find(".for-add").text(name);
+            var $editDiv = $newProd.find('.editDiv');
+            var $nameDiv = $newProd.find(".for-add");
+            $editDiv.hide();
+            $nameDiv.text(name);
             $('.left').append($newProd);
 
             //clean the input
@@ -22,17 +26,17 @@ $(function () {
             $boughtProd.removeAttr('id');
             $boughtProd.find('.itemName').text(name);
             $('#crossedProd').append($boughtProd);
+            $boughtProd.hide();
 
 
             //event on minus button
             var $minus = $newProd.find('.minBut');
             $minus.click(function () {
                 count--;
-                if(count==1){
+                if (count == 1) {
                     $minus.css('background-color', 'rgba(226, 61, 61, 0.6)');
                     $minus.css('cursor', 'auto');
-                }
-                else if(count<1)
+                } else if (count < 1)
                     count++;
                 $newProd.find('.leftamount').text(count);
                 $prodLeft.find('.rightamount').text(count);
@@ -41,55 +45,71 @@ $(function () {
             });
             //event on plus button
             var $plus = $newProd.find('.plusbut');
-            $plus.click(function(){
-               count++;
-               $newProd.find('.leftamount').text(count);
-               $prodLeft.find('.rightamount').text(count);
+            $plus.click(function () {
+                count++;
+                $newProd.find('.leftamount').text(count);
+                $prodLeft.find('.rightamount').text(count);
                 $boughtProd.find('.rightamount').text(count);
-               if(count>1){
-                   $minus.css('background-color', 'rgb(226, 61, 61)');
-                   $minus.css('cursor', 'pointer');
-               }
+                if (count > 1) {
+                    $minus.css('background-color', 'rgb(226, 61, 61)');
+                    $minus.css('cursor', 'pointer');
+                }
             });
 
             //event on del button
-            var $del =$newProd.find('.delbut');
-            if($del){
-              $del.click(function () {
-                  $newProd.remove();
-                  $prodLeft.remove();
-                  $boughtProd.remove();
-              });
+            var $del = $newProd.find('.delbut');
+            if ($del) {
+                $del.click(function () {
+                    $newProd.remove();
+                    $prodLeft.remove();
+                    $boughtProd.remove();
+                });
             }
 
             //event on the bought button
             var boughtButCount = 1;
             var $boughtBut = $newProd.find('.boughtbut');
-            if($boughtBut){
+            if ($boughtBut) {
                 $boughtBut.click(function () {
-                    if(boughtButCount) {
-                        $newProd.find(".for-add").css('text-decoration', 'line-through');
-                        $minus.css('display', 'none');
-                        $plus.css('display', 'none');
-                        $del.css('display', 'none');
+                    if (boughtButCount) {
+                        $nameDiv.css('text-decoration', 'line-through');
+                        $minus.hide();
+                        $plus.hide();
+                        $del.hide();
                         $boughtBut.text('Не куплено');
-                        $boughtProd.css('display', 'inline-block');
-                        $boughtProd.find('.rightamount').css('display', 'inline-block');
-                        $prodLeft.css('display', 'none');
+                        $boughtProd.show();
+                        $boughtProd.find('.rightamount').show();
+                        $prodLeft.hide();
                         boughtButCount--;
-                    }
-                    else{
-                        $newProd.find(".for-add").css('text-decoration', 'none');
-                        $minus.css('display', 'inline-block');
-                        $plus.css('display', 'inline-block');
-                        $del.css('display', 'inline-block');
+                    } else {
+                        $nameDiv.css('text-decoration', 'none');
+                        $minus.show();
+                        $plus.show();
+                        $del.show();
                         $boughtBut.text('Куплено');
-                        $boughtProd.css('display', 'none');
-                        $prodLeft.css('display', 'inline-block');
+                        $boughtProd.hide();
+                        $prodLeft.show();
                         boughtButCount++;
                     }
                 });
             }
+
+            //editing the prodname
+            var $editName = $editDiv.find('#editName');
+            $nameDiv.click(function () {
+                $nameDiv.hide();
+                $editDiv.show();
+                $editName.focus();
+                $editName.val($nameDiv.text());
+                $editName.blur(function () {
+                    $editDiv.hide();
+                    $nameDiv.show();
+                    $nameDiv.text($editName.val());
+                    $prodLeft.find('.right-prodName').text($editName.val());
+                    $boughtProd.find('.itemName').text($editName.val());
+                });
+            });
+
         }
     }
 
@@ -104,6 +124,8 @@ $(function () {
         if (e.which == 13) {
             var prodName = $('.nameinput').val();
             addProd(prodName);
+        } else if (e.which == 32) { //prevent space key
+            e.preventDefault();
         }
     });
 
